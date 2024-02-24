4aee8205eca4df4e30501f0d43f4dcd4167a3d9c
O-ID (Ody) <odydahary1@gmail.com>
2024-02-24 15:09:50 +0700

    update again

--- server/routes/hello.js
+++ server/routes/hello.js
@@ -1,10 +1,11 @@
 import { drizzle } from "drizzle-orm/d1"
 
 export default defineEventHandler( async (event) => {
-    const db = drizzle(process.env.DB)
+    // const db = drizzle(process.env.DB)
     console.log(process.env);
     console.log(process);
-    const hasil = await db.select().from(tabletest).all()
+    // const hasil = await db.select().from(tabletest).all()
 
+    const hasil = 'hello'
     return Response.json(hasil)
 })
